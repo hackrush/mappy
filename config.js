eda96903b432c12bec8325a5487c4b2f8ae7b602
Denzel Wamburu <denzjoseph@gmail.com>
2016-06-23 21:48:25 +0300

    Fixed mapId

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var config = {
   firebase: 'mappy-1570c',
   mapbox: {
       accessToken: 'pk.eyJ1IjoiZGVuemVsd2FtYnVydSIsImEiOiJjaW1xOTBoeHEwMDFtdnpseThjdmNmcm5lIn0.uJ-GsqtY0UjKZYdgAR63LA',
-      mapId: '',
+      mapId: 'cipsmettv002adnncwhlhir55',
   }
 }

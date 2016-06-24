a4005078c163f6a14a6ad267a95e086456f728bb
Bakley <koin254@cheap-trills.mshome.net>
2016-06-24 23:37:15 +0300

    Use satellite

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
-  firebase: 'mappy-1570c',
+  firebase: 'my-road-map',
   mapbox: {
-      accessToken: 'pk.eyJ1IjoiZGVuemVsd2FtYnVydSIsImEiOiJjaW1xOTBoeHEwMDFtdnpseThjdmNmcm5lIn0.uJ-GsqtY0UjKZYdgAR63LA',
-      mapId: 'cipsmettv002adnncwhlhir55',
+      accessToken: 'pk.eyJ1IjoiYmFrbGV5IiwiYSI6ImNpcHU0enZlazAwNDRoem0yamtkZTd1bWMifQ.r2_StP_qOjVh0rEnkazg3Q',
+      mapId: 'mapbox.streets-satellite',
   }
 }
